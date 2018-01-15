9dcb70be1a29815ada8a53cd650a70c288b61425
Roman Varenk <rvarenik@cryptology.com>
2018-01-15 15:34:22 +0300

    version up

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 6):
 
 setup(
     name='cryptology-client-python',
-    version='0.2.1',
+    version='0.3.0',
     description='cryptology webscoket client',
     author='Cryptology',
     author_email='victor@cryptology.com',
